c2782a134110bf82ca93861a2af9406d80874852
Bappi Mazumder <mazumderbappi@gmail.com>
2024-10-12 11:47:40 +0600

    add log back file  for log visualization

--- src/main/java/com/bappi/restaurantmanagement/controller/CustomerController.java
+++ src/main/java/com/bappi/restaurantmanagement/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.bappi.restaurantmanagement.model.dto.CustomerResponseDto;
 import com.bappi.restaurantmanagement.service.CustomerService;
 import com.bappi.restaurantmanagement.utils.ResponsePayload;
 import com.bappi.restaurantmanagement.utils.ServiceExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -12,11 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 import static com.bappi.restaurantmanagement.config.ApiPath.API_CUSTOMER;
 import static com.bappi.restaurantmanagement.config.ApiPath.API_GET_ALL_CUSTOMER;
-
+@Slf4j
 @RestController
 @RequestMapping(API_CUSTOMER)
 public class CustomerController {
@@ -29,6 +28,7 @@ public class CustomerController {
 
     @GetMapping(value = API_GET_ALL_CUSTOMER)
     public HttpEntity<ResponsePayload<CustomerResponseDto>> getAllCustomers() {
+        log.info("Call Start getAllCustomers() method");
         ServiceExceptionHandler<ResponsePayload<CustomerResponseDto>> dataHandler = service::getAllCustomers;
         ResponsePayload<CustomerResponseDto> dto = dataHandler.executeHandler();
         return new ResponseEntity<>(dto, HttpStatus.OK);
--- src/main/java/com/bappi/restaurantmanagement/controller/OrderController.java
+++ src/main/java/com/bappi/restaurantmanagement/controller/OrderController.java
@@ -4,11 +4,8 @@ package com.bappi.restaurantmanagement.controller;
 import com.bappi.restaurantmanagement.model.dto.OrderResponseDto;
 import com.bappi.restaurantmanagement.model.dto.SaleResponseDto;
 import com.bappi.restaurantmanagement.model.entity.Customer;
-import com.bappi.restaurantmanagement.model.entity.Order;
 import com.bappi.restaurantmanagement.service.CustomerService;
 import com.bappi.restaurantmanagement.service.OrderService;
-import com.bappi.restaurantmanagement.utils.APIErrorCode;
-import com.bappi.restaurantmanagement.utils.CustomException;
 import com.bappi.restaurantmanagement.utils.ResponsePayload;
 import com.bappi.restaurantmanagement.utils.ServiceExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Objects;
 
 import static com.bappi.restaurantmanagement.config.ApiPath.*;
@@ -43,6 +39,7 @@ public class OrderController {
 
     @GetMapping(value=API_GET_ALL_ORDER_TODAY)
     public HttpEntity<ResponsePayload<OrderResponseDto>> getTodayOrders() {
+        log.info("Call Start getTodayOrders() method");
         ServiceExceptionHandler<ResponsePayload<OrderResponseDto>> dataHandler = service :: getTodayOrders;
         ResponsePayload<OrderResponseDto> dto = dataHandler.executeHandler();
         return new ResponseEntity<>(dto, HttpStatus.OK);
@@ -50,6 +47,7 @@ public class OrderController {
 
     @GetMapping(value = API_GET_ALL_SALE_TODAY)
     public HttpEntity<?> getTodayTotalSaleAmount(){
+        log.info("Call Start getTodayTotalSaleAmount() method");
         ServiceExceptionHandler<SaleResponseDto> dataHandler = service :: getTodayTotalSaleAmount;
         SaleResponseDto dto = dataHandler.executeHandler();
         return new ResponseEntity<>(dto, HttpStatus.OK);
@@ -57,7 +55,7 @@ public class OrderController {
 
     @GetMapping(value=API_GET_ALL_ORDER_BY_CUSTOMER)
     public HttpEntity<ResponsePayload<OrderResponseDto>> getOrdersByCustomer(@RequestParam(value = "customerCode",required = true) String customerCode) {
-
+        log.info("Call Start getOrdersByCustomer() method for customer {}",customerCode);
         ResponsePayload<OrderResponseDto> dto = null;
         Customer customer = customerService.getCustomer(customerCode);
         if(Objects.isNull(customer)){
@@ -73,6 +71,7 @@ public class OrderController {
     @GetMapping(value=API_GET_MAX_SALE_A_DAY)
     public HttpEntity<?> getMaxSaleDay(@RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
                                         @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
+        log.info("Call Start getMaxSaleDay() method for start {} and end {}",start,end);
         ServiceExceptionHandler<SaleResponseDto> dataHandler = () -> service.getMaxSaleDay(start, end);
         SaleResponseDto dto = dataHandler.executeHandler();
         return new ResponseEntity<>(dto, HttpStatus.OK);
